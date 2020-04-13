e7f867b54137853d24515b09b33baa151c42d20d
tyaremchuk <mirracke_1123581321>
2020-04-13 08:45:29 +0300

    thats a branch update commit

--- src/learn_pckg1/Head.java
+++ src/learn_pckg1/Head.java
@@ -29,7 +29,7 @@ public class Head extends JFrame
     {
         f = new JFrame("Frame of smthing");
         JEditorPane editorPane = new JEditorPane();
-        editorPane.setContentType("text/html");
+        editorPane.setContentType("text/plain");
         editorPane.setText("<h1>Sleeping</h1><p>Sleeping is necessary for a healthy body."
                 + " But sleeping in unnecessary times may spoil our health, wealth and studies."
                 + " Doctors advise that the sleeping at improper timings may lead for obesity during the students days.</p>");
